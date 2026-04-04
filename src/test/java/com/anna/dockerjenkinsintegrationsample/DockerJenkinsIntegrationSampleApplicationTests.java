883417767823027c87877a1b31461bc2c01dc61b
annamalai <1987annamalai@gmail.com>
2026-04-04 21:36:16 +0530

    first Commit

---
+++ src/test/java/com/anna/dockerjenkinsintegrationsample/DockerJenkinsIntegrationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anna.dockerjenkinsintegrationsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
